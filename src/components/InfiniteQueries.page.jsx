ca1a498709be2e5fdb28747e0e6488ff6cf8897a
Mladen <mladenvts@gmail.com>
2022-11-23 13:21:53 +0100

    Implement InfiniteQuery with React Query

---
+++ src/components/InfiniteQueries.page.jsx
@@ -0,0 +1,69 @@
+import { useInfiniteQuery } from "react-query";
+import axios from "axios";
+import { Fragment } from "react";
+
+const fetchColors = ({ pageParam = 1 }) => {
+  return axios.get(`http://localhost:4000/colors?_limit=2&_page=${pageParam}`);
+};
+
+export const InfiniteQueriesPage = () => {
+  const {
+    isLoading,
+    isError,
+    error,
+    data,
+    hasNextPage,
+    fetchNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery(["colors"], fetchColors, {
+    getNextPageParam: (_lastPage, pages) => {
+      if (pages.length < 4) {
+        return pages.length + 1;
+      } else {
+        return undefined;
+      }
+    },
+  });
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+  if (isError) {
+    return <h2>{error.message}</h2>;
+  }
+
+  return (
+    <>
+      <div>
+        {/* {data?.data.map((item) => {
+          return (
+            <div key={item.id}>
+              <h4>
+                {item.id}. {item.label}
+              </h4>
+            </div>
+          );
+          
+        })} */}
+        {data?.pages.map((group, i) => {
+          return (
+            <Fragment key={i}>
+              {group.data.map((color) => (
+                <h2 key={color.id}>
+                  {color.id} {color.label}
+                </h2>
+              ))}
+            </Fragment>
+          );
+        })}
+      </div>
+      <div>
+        <button disabled={!hasNextPage} onClick={fetchNextPage}>
+          Load more
+        </button>
+      </div>
+      <div>{isFetching && !isFetchingNextPage ? "Fetching..." : null}</div>
+    </>
+  );
+};
