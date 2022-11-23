f91ff0e9472d6ae550372a88a618665bac0effe7
Mladen <mladenvts@gmail.com>
2022-11-23 11:29:15 +0100

    DependentQueries

---
+++ src/components/DependentQueries.page.jsx
@@ -0,0 +1,35 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchUserByEmail = (email) => {
+  return axios.get(`http://localhost:4000/users/${email}`);
+};
+
+const fetchCoursesByChannelId = (channelId) => {
+  return axios.get(`http://localhost:4000/channels/${channelId}`);
+};
+
+export const DependentQueriesPage = ({ email }) => {
+  const { data: user } = useQuery(["user", email], () =>
+    fetchUserByEmail(email)
+  );
+  const channelId = user?.data.channelId;
+
+  const { data: courses } = useQuery(
+    ["courses", channelId],
+    () => fetchCoursesByChannelId(channelId),
+    {
+      enabled: !!channelId,
+    }
+  );
+
+  /* coursess?.data.courses.map((course) => console.log(course)); */
+
+  return (
+    <div>
+      {courses?.data.courses.map((item, index) => {
+        return <div key={index}>{item}</div>;
+      })}
+    </div>
+  );
+};
